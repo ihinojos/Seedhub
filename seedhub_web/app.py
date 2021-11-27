ebf018c5d2dbf2c690a9800465aa665d99d44e2b
Roxana <roxanaLimondelarosa@gmail.com>
2021-11-27 00:17:46 -0700

    AddRutas

--- seedhub_web/app.py
+++ seedhub_web/app.py
@@ -3,12 +3,28 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return render_template('index.html')
-    
+@app.route('/registrar')
+def registrar():    
+    return render_template('Registrar.html')
+
+@app.route('/')  
+def login():
+    return render_template('Login.html')
 if __name__ == "__main__":
     app.run(debug=True)
 
+@app.route('/miperfil')  
+def miperfil():
+    return render_template('MiPerfil.html')
 
+@app.route('/infousuario')  
+def infousuarito():
+    return render_template('InfoUsuario.html')
 
+@app.route('/estadistica')  
+def estadistica():
+    return render_template('Estadistica.html')    
+
+@app.route('/index')  
+def index():
+    return render_template('index.html')    
