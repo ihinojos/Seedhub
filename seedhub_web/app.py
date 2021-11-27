14b3dc6bf54da3e4f8dbcc2cc7a1fae4a0e612a7
Isaí Hinojos <hinojosisai@gmail.com>
2021-11-27 16:07:22 -0700

    added serial server

--- seedhub_web/app.py
+++ seedhub_web/app.py
@@ -1,13 +1,38 @@
-import re
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify, request
 
 app = Flask(__name__)
+cmd_queue = []
 
 @app.route('/')
 def index():
     
     return render_template('index.html')
-    
+
+@app.route('/api/hello')
+def hello():
+    return jsonify({"message":"Hello, world"})
+
+@app.route('/api/save_info', methods=['POST'])
+def save_info():
+    plant_info = request.get_json()
+    print(plant_info)
+    return jsonify(plant_info)
+
+@app.route('/serial/toggle_leds')
+def toggle_leds():
+    cmd_queue.append('<toggle_leds,0>')
+    return jsonify("OK")
+
+@app.route('/serial/get_cmd')
+def get_command():
+    cmd = None
+    try:
+        cmd = cmd_queue.pop(0)
+    except:
+        cmd = "None"
+    return jsonify({"cmd":cmd})
+
+
 if __name__ == "__main__":
     app.run(debug=True)
 
